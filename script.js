4616d8b8eb8c262503bdf254abcf3174f52798a7
SamuelMielnik <samuelmielnik21@gmail.com>
2026-06-16 11:15:31 -0300

    Pegando produtos do servidor

---
+++ script.js
@@ -0,0 +1,9 @@
+const searchForm = document.querySelector(".search-form")
+
+searchForm.addEventListener("submit", async function(event){
+    event.preventDefault()
+    const inputValue = event.target[0].value
+
+    const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${inputValue}`)
+    const products = (await data.json()).results.slice(0, 10)
+})
